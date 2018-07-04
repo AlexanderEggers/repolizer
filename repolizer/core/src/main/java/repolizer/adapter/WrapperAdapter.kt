28f7614031e22ef27cc0cc430627b035d5ead31e
Mordag <alexander.eggers91@gmail.com>
2018-07-04 23:36:57 +1000

    Tweaking WrapperAdapter

--- repolizer/core/src/main/java/repolizer/adapter/WrapperAdapter.kt
+++ repolizer/core/src/main/java/repolizer/adapter/WrapperAdapter.kt
@@ -1,11 +1,10 @@
 package repolizer.adapter
 
 import repolizer.repository.future.Future
+import repolizer.repository.network.NetworkFuture
 import java.lang.reflect.Type
 
 interface WrapperAdapter<O> {
 
-    fun getType(): Type
-
-    fun <B> execute(future: Future<O, B>): O
+    fun <B> execute(future: NetworkFuture<B>): O
 }
