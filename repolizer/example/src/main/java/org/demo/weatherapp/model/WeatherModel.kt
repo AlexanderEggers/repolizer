7596c33faeca4b3f6f91b1758542ba4b23a4f90d
Mordag <alexander.eggers91@gmail.com>
2018-07-19 20:20:48 +1000

    Fixed some more build errors

--- repolizer/example/src/main/java/org/demo/weatherapp/model/WeatherModel.kt
+++ repolizer/example/src/main/java/org/demo/weatherapp/model/WeatherModel.kt
@@ -1,15 +1,9 @@
 package org.demo.weatherapp.model
 
-import android.arch.persistence.room.ColumnInfo
-import android.arch.persistence.room.Embedded
-import android.arch.persistence.room.Entity
-import android.arch.persistence.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
-@Entity(tableName = "weather_data")
 class WeatherModel {
 
-    @PrimaryKey
     @SerializedName("id")
     var id: Int = 0
 
@@ -19,15 +13,12 @@ class WeatherModel {
     @SerializedName("weather")
     var condition: ArrayList<ConditionModel>? = null
 
-    @ColumnInfo(name = "data_time")
     @SerializedName("dt")
     var dataTime: Long = 0
 
-    @Embedded
     @SerializedName("main")
     var data: DataModel? = null
 
-    @Embedded
     @SerializedName("sys")
     var systemData: SystemModel? = null
 }
