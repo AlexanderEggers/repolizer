c63d68ef781bb3fc9697f43005c93d6697084b2d
Alexander Eggers <alex@domesticcat.com.au>
2019-05-07 17:04:34 +1000

    Fixed missing builder hooks

--- repolizer/core/src/main/java/repolizer/repository/persistent/PersistentFutureRequest.kt
+++ repolizer/core/src/main/java/repolizer/repository/persistent/PersistentFutureRequest.kt
@@ -6,8 +6,8 @@ import repolizer.repository.future.FutureRequest
 
 open class PersistentFutureRequest(builder: PersistentFutureRequestBuilder) : FutureRequest(builder) {
 
-    var cacheOperation: CacheOperation? = null
-    var dataOperation: DataOperation? = null
+    var cacheOperation: CacheOperation? = builder.cacheOperation
+    var dataOperation: DataOperation? = builder.dataOperation
 
     var cacheObject = builder.cacheObject
     var dataObject = builder.dataObject
